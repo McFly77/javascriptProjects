49c3fa1062d528d9d68c4bf6e936ce05a142e530
martin <martinm2011@live.ca>
2025-01-06 15:56:24 -0500

    math functions

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -14,7 +14,18 @@ const address = {
   state: 'MA'
 }
 val = String(new Date());
+//toString()
+val = (5).toString();
+val = (true).toString();
+
+// String to number
+val = Number('5');
+
+val = parseInt('100.30');
+val = parseFloat('100.25');
 
 console.log(val);
 console.log(typeof val);
 console.log(val.length);
+console.log(val.toFixed(2));
+
---
+++ js_sandbox/mathFunctions/app.js
@@ -0,0 +1,27 @@
+const num1 = 100;
+const num2 = 50;
+
+let val;
+
+//simple math
+
+val = num1 + num2 
+val = num1 % num2
+
+  // Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(3.7869);
+val = Math.ceil(3.6);
+val = Math.floor(3.6);
+val = Math.sqrt(64);
+val = Math.abs(-67);
+val = Math.pow(4, 2);
+val = Math.min(3, 7, 13, -34, 70);
+val = Math.max(3, 7, 13, -34, 70);
+
+val = Math.floor(Math.random() * 20 + 1);
+
+
+
+console.log(val);
