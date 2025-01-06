d3253bf0f8207c15194af8bbdaec72975ba74511
martin <martinm2011@live.ca>
2025-01-06 07:43:00 -0500

    add foler js_sand_box

---
+++ js_sandbox/app.js
@@ -0,0 +1,20 @@
+// PRIMITIVE
+
+const name = 'John Doe';
+console.log(typeof name);
+
+//ARRAY
+
+const hobbies = ['movies', 'music'];
+
+
+
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+val = String(new Date());
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
